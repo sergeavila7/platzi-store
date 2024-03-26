9368f75b62df6531ac2b68d79bde08ebebaab1c1
sergeavila7 <serge_avila7@yahoo.com>
2024-03-26 03:04:39 -0600

    fix: imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './controllers/products.controller';
-import { CategoriesController } from './controllers/categories.controller';
-import { ProductsModule } from './nest/products/products.module';
+import { ProductsController } from './products/products.controller';
+import { CategoriesController } from './categories/categories.controller';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { OrdersModule } from './orders/orders.module';
 import { UsersModule } from './users/users.module';
-import { UsersService } from './controllers/users/users.service';
+import { UsersService } from './users/users.service';
 import { CustomersModule } from './customers/customers.module';
 import { BrandsModule } from './brands/brands.module';
 
 @Module({
-  imports: [ProductsModule, CategoriesModule, OrdersModule, UsersModule, CustomersModule, BrandsModule],
+  imports: [
+    ProductsModule,
+    CategoriesModule,
+    OrdersModule,
+    UsersModule,
+    CustomersModule,
+    BrandsModule,
+  ],
   controllers: [AppController, ProductsController, CategoriesController],
   providers: [AppService, UsersService],
 })
