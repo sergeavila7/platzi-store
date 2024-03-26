fa793d93ca3b6e182e6138fe5222fa02a4e9146d
sergeavila7 <serge_avila7@yahoo.com>
2024-03-26 03:02:54 -0600

    add: categories endpoint

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  @Get(':categoryId/products/:productId')
+  getCategory(@Param() { categoryId, productId }) {
+    return `Product ${productId}, Category ${categoryId}`;
+  }
+}
