e691e595a8fbc4f554bfc00e59df4584dde153d1
sergeavila7 <serge_avila7@yahoo.com>
2024-03-26 02:56:58 -0600

    add: users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { AppService } from './app.service';
 import { ProductsController } from './controllers/products.controller';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsModule } from './nest/products/products.module';
-import { ProductsModule } from './controllers/products/products.module';
-import { CategoriesModule } from './controllers/categories/categories.module';
-import { OrdersModule } from './controllers/orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { OrdersModule } from './orders/orders.module';
+import { UsersModule } from './users/users.module';
+import { UsersService } from './controllers/users/users.service';
 
 @Module({
-  imports: [ProductsModule, CategoriesModule, OrdersModule],
+  imports: [ProductsModule, CategoriesModule, OrdersModule, UsersModule],
   controllers: [AppController, ProductsController, CategoriesController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
